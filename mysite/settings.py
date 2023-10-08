d060e3389d2a577430c0fda84afb088ae02cdf16
Ben Wang <benwang@Bens-MacBook-Pro.local>
2023-10-07 22:38:15 -0400

    edit allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,11 +31,11 @@ if not IS_HEROKU_APP:
     DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['local host', 'https://b21-app-9b430d776820.herokuapp.com', '127.0.0.1']
-# if IS_HEROKU_APP:
-#     ALLOWED_HOSTS = ['local host', 'https://b21-app-9b430d776820.herokuapp.com']
-# else:
-#     ALLOWED_HOSTS = ['local host', 'https://b21-app-9b430d776820.herokuapp.com', '127.0.0.1']
+
+if IS_HEROKU_APP:
+    ALLOWED_HOSTS = ['localhost', 'https://b21-app-9b430d776820.herokuapp.com','.herokuapp.com']
+else:
+    ALLOWED_HOSTS = ['localhost', 'https://b21-app-9b430d776820.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
